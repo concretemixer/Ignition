d201a5c04872d94df31f81039f4cc78008c1da39
max <max.pervukhin@gmail.com>
2016-12-16 02:52:32 +0700

    mega1

---
+++ Assets/CarCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarCamera : MonoBehaviour {
+
+    public GameObject Target;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Target.transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Target.transform.rotation, Time.deltaTime);
+	}
+}
---
+++ Assets/MeshCombiner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshCombiner : MonoBehaviour {
+
+	// Use this for initialization
+	public void Go () {
+
+        Quaternion q = transform.rotation;
+        Vector3 p = transform.position;
+
+        transform.rotation = Quaternion.identity;
+        transform.position = Vector3.zero;
+
+        MeshFilter[] filters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] c = new CombineInstance[filters.Length];
+
+
+
+        for(int a=0;a<filters.Length;a++)
+        {
+            c[a] = new CombineInstance();
+            c[a].subMeshIndex = 0;
+            c[a].mesh = filters[a].sharedMesh;
+            c[a].transform = filters[a].transform.localToWorldMatrix;
+        }
+
+
+        Mesh mesh = new Mesh();
+
+        mesh.CombineMeshes(c);
+
+
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+
+        transform.rotation = q;
+        transform.position = p;
+	}
+}
